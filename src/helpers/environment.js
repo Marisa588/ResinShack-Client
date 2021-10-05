d1447ecdff3c0c7522f8cf6bab371ca4397a2314
Marisa588 <79545016+MarMar3383@users.noreply.github.com>
2021-10-05 15:05:27 -0400

    update APIURL

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,10 +2,10 @@ let APIURL = '';
 
 switch(window.location.hostname) {
     case 'localhost' || '127.0.0.1':
-        APIURL ='http://localhost:5432';
+        APIURL ='http://localhost:3001';
         break;
     case 'julees-resin-shack-client.herokuapp.com':
-        APIURL = 'https://julees-resin-shack-client.herokuapp.com'
+        APIURL = 'https://julees-resin-shack-server.herokuapp.com'
 }
 
 export default APIURL;
